6f0fc3315f32c9de8be78b637651bd3516aa709f
Abdullah0889 <118253113+Abdullah0889@users.noreply.github.com>
2025-08-25 14:42:23 +0530

    Atlas

--- db/config.js
+++ db/config.js
@@ -1,2 +1,11 @@
+// db/config.js
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/e-commDash')
+const dotenv = require("dotenv");
+dotenv.config();
+const mongoURI = process.env.MONGO_URI || "mongodb://localhost:27017/e-commDash";
+
+mongoose.connect(mongoURI)
+  .then(() => console.log("✅ MongoDB Connected"))
+  .catch((err) => console.error("❌ MongoDB Error:", err));
+
+module.exports = mongoose;
